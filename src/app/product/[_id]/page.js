d7358e82f6299a6c94df8289d2981ed2b8e37469
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-09-23 03:12:57 +0600

    Update page.js

--- src/app/product/[_id]/page.js
+++ src/app/product/[_id]/page.js
@@ -1,10 +1,10 @@
-export default async function Page({ params: { _id } }) {
+lexport default async function Page({ params: { _id } }) {
   try {
-    const response = await fetch(`https://divinemenswear.com/api/admin/products/${_id}`);
+    const response = await fetch(`/api/admin/products/${_id}`);
     
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
-    }
+   
 
     const data = await response.json();
     console.log(data); // log the data for debugging
