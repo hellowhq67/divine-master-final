c4c1e5d0b255c26337c7ee6be0ebac765c8da59e
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-09-23 13:55:46 +0600

    Update page.js

--- src/app/product/[_id]/page.js
+++ src/app/product/[_id]/page.js
@@ -5,11 +5,11 @@ import Nav from "@/components/Navbar/Nav";
 import Footer from "@/components/Footer/Footer";
 
 // Server Component (data fetching is handled on the server side)
-export default async function ProductPage({ params }) {
-  const { id } = params; // access the dynamic route parameter
+export default async function ProductPage({ params:_id }) {
+  // access the dynamic route parameter
 
   try {
-    const response = await axios.get(`/api/admin/products/${id}`);
+    const response = await axios.get(`/api/admin/products/${_id}`);
     const product = response.data.products;
 
     return (
