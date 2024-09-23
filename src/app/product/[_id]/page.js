5999bb260c1760cc16d90f556a1c476f0dcc7e85
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-09-24 01:13:41 +0600

    Update page.js

--- src/app/product/[_id]/page.js
+++ src/app/product/[_id]/page.js
@@ -1,9 +1,9 @@
-'use server'
+
 import Nav from "@/components/Navbar/Nav";
 import React from "react";
 import ProductDetail from "@/components/Product Details/ProductDetail";
 import Footer from "@/components/Footer/Footer";
-
+export const dynamic = "force-dynamic";
 export default async function Page({ params: { _id } }) {
   const response = await fetch(`/api/admin/products/${_id}`);
   const data = await response.json();
