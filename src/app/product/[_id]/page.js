5c6f9b35bf306a9112e705ce09f47c155f3096c8
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-09-24 01:03:20 +0600

    Update page.js

--- src/app/product/[_id]/page.js
+++ src/app/product/[_id]/page.js
@@ -1,26 +1,19 @@
-import axios from "axios";
-
-import ProductDetail from "@/components/Product Details/ProductDetail";
+'use server'
 import Nav from "@/components/Navbar/Nav";
+import React from "react";
+import ProductDetail from "@/components/Product Details/ProductDetail";
 import Footer from "@/components/Footer/Footer";
 
-// Server Component (data fetching is handled on the server side)
-export default async function ProductPage({ params:_id }) {
-  // access the dynamic route parameter
+export default async function Page({ params: { _id } }) {
+  const response = await fetch(`/api/admin/products/${_id}`);
+  const data = await response.json();
+  const product = data.products;
 
-  try {
-    const response = await axios.get(`/api/admin/products/${_id}`);
-    const product = response.data.products;
-
-    return (
-      <div>
-        <Nav />
-        <ProductDetail product={product} />
-        <Footer />
-      </div>
-    );
-  } catch (error) {
-    console.error("Error fetching product data:", error);
-    return <div>Failed to load product details.</div>;
-  }
+  return (
+    <div>
+      <Nav />
+      <ProductDetail product={product} />
+      <Footer/>
+    </div>
+  );
 }
