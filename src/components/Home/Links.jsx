da3cf242c556dadc701ff903f3fbde16784c1e6c
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-10-12 18:58:33 +0600

    Update Links.jsx

--- src/components/Home/Links.jsx
+++ src/components/Home/Links.jsx
@@ -1,8 +1,6 @@
 import { Link } from '@mui/material'
 import React from 'react'
-
-export default function Links() {
-    const data =[
+const data =[
         {
             name:" T-Shirts",
             link:"/category/tops"
@@ -35,11 +33,13 @@ export default function Links() {
         },
         
     ]
+export default function Links() {
+    
   return (
-    <div className='flex w-screen flex-col lg:h-80 items-start  my-20 lg:px-8 '>
+    <div className='flex w-screen flex-col lg:h-80 items-start  my-20 px-2 lg:px-0 '>
       <h1 className='text-3xl my-10 font-semibold font-sans ' >POPULAR RIGHT NOW</h1>
 
-      <div className="flex w-screen flex-wrap gap-20 lg:h-screen">
+      <div className="flex  w-screen flex-wrap gap-20 sm:h-full lg:h-screen">
         {data.map((x)=>{
             return(
                 <Link style={{width:'250px'}} className='h-10 text-2xl font-bold  text-black no-underline border-b-2 border-black hover:border-b-4 font-sans' href={x.link}>
