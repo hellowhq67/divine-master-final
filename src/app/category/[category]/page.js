282df9407cfa2ded752bcea099df3d89df5ed9f0
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-10-17 23:23:59 +0600

    Update page.js

--- src/app/category/[category]/page.js
+++ src/app/category/[category]/page.js
@@ -1,10 +1,10 @@
-import Nav from '@/components/Navbar/Nav'
+import Nav from '@/components/Navbar/Navigation'
 import React from 'react'
 import Products from '@/components/Cetagory/Products'
 export default function page({ params: { category } }) {
   return (
     <div>
-      <Nav/>
+      <Navigation/>
       <Products  category={category}/>
     </div>
   )
