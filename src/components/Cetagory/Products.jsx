e8d31369f360ef56f26fed4e54143db9101d9a28
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-10-17 23:39:45 +0600

    Update Products.jsx

--- src/components/Cetagory/Products.jsx
+++ src/components/Cetagory/Products.jsx
@@ -1,19 +1,4 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
 "use client";
-
 import { useState, useEffect } from "react";
 import styles from "./style.module.css";
 import {
