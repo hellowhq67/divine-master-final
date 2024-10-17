08ecdb3c3d7d9fe82523c4bf6c6b450dd4c6adca
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-10-17 23:22:44 +0600

    Update page.js

--- src/app/product/[_id]/page.js
+++ src/app/product/[_id]/page.js
@@ -3,7 +3,9 @@ import Nav from "@/components/Navbar/Nav";
 import React from "react";
 import ProductDetail from "@/components/Product Details/ProductDetail";
 import Footer from "@/components/Footer/Footer";
+import Navigation from '@/components/Navbar/Navigation'
 export const dynamic = "force-dynamic";
+
 export default async function Page({ params: { _id } }) {
   const response = await fetch(`https://divinemenswear.com/api/admin/products/${_id}`);
   const data = await response.json();
@@ -11,7 +13,7 @@ export default async function Page({ params: { _id } }) {
 
   return (
     <div>
-      <Nav />
+      <Navigation/>
       <ProductDetail product={product} />
       <Footer/>
     </div>
