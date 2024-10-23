aac90bdcda726d75098e6b6b35dba98fc360f396
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-10-24 01:05:33 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,22 @@ const nextConfig = {
   reactStrictMode: true,
   cacheHandler: resolve('./cache-handler.js'),
   cacheMaxMemorySize: 0,
+
+  // Add the headers function for CORS
+  async headers() {
+    return [
+      {
+        // This will apply the headers to all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://divinemenswear.com/" }, // You can replace "*" with your domain if needed
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
