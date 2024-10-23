c36dcfd6b3deab9b5272642d5738a1fdd9614202
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-10-23 21:35:23 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,9 @@ import Footer from "@/components/Footer/Footer";
 import Banner2 from "@/components/Home/Banner2";
 import Banner3 from "@/components/Home/Banner3";
 import Banner4 from "@/components/Home/Banner4";
-
 import Fetured from "@/components/Fetured/Fetured";
 import Navigation from '@/components/Navbar/Navigation'
 import Links from '@/components/Home/Links'
-import MessengerCustomerChat from 'react-messenger-customer-chat';
 export default function Home() {
 
 
@@ -30,11 +28,7 @@ export default function Home() {
       </div>
      
       <Footer />
-      <MessengerCustomerChat
-    pageId="61559415209737"
-    appId="3721280451471835"
- 
-  />,
+    
     </>
   );
 }
