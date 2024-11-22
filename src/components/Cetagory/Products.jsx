d6fb1665295e66d4dbc862039ef8ed0cf2577d4b
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-11-22 23:57:22 +0600

    Update Products.jsx

--- src/components/Cetagory/Products.jsx
+++ src/components/Cetagory/Products.jsx
@@ -699,65 +699,44 @@ export default function Products({category}) {
                       <>
                         {filteredProducts.map((product) => {
                           return (
-                            <Link
-                              href={`/product/${product._id}`}
-                              class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
-                            >
-                              <Link href={`/product/${product._id}`}>
+                            <div className={styles.Card}>
+                              <Link
+                                href={`/product/${product._id}`}
+                                className={styles.productImgae}
+                              >
                                 <img
-                                  class="h-80 w-72 object-cover rounded-t-xl"
                                   src={product.productImage1}
-                                  alt="image"
+                                  alt={product.productName}
                                 />
-
-                                <div class="px-4 py-3 w-72">
-                                  <span class="text-gray-400 mr-3 uppercase text-xs">
-                                    DIVINE
-                                  </span>
-                                  <p class="text-lg font-bold text-black truncate block capitalize">
-                                    {product.productName.slice(0, 21)}...
-                                  </p>
-                                  <div class="flex items-center">
-                                    {!product.smartPrice ? (
-                                      <p class="text-lg font-semibold text-black cursor-auto my-3">
-                                        {product.price} BTD
-                                      </p>
-                                    ) : (
-                                      <>
-                                        <p class="text-lg font-semibold text-black cursor-auto my-3">
-                                          {product.smartPrice}BTD
-                                        </p>
-                                        <del>
-                                          <p class="text-sm text-gray-600 cursor-auto ml-2">
-                                            {product.price} BTD
-                                          </p>
-                                        </del>
-                                      </>
-                                    )}
-
-                                    <button
-                                      onClick={() => handleAdd(product)}
-                                      class="ml-auto"
-                                    >
-                                      <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="20"
-                                        height="20"
-                                        fill="currentColor"
-                                        class="bi bi-bag-plus"
-                                        viewBox="0 0 16 16"
-                                      >
-                                        <path
-                                          fill-rule="evenodd"
-                                          d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                                        />
-                                        <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                                      </svg>
-                                    </button>
-                                  </div>
-                                </div>
+                                <span
+                                  className={styles.discount}
+                                >{`${Math.round(
+                                  ((product.price - product.smartPrice) /
+                                    product.price) *
+                                    100
+                                )}% off`}</span>
                               </Link>
-                            </Link>
+                              <div className={styles.ProductInfo}>
+                                <Link href={`/product/${product._id}`} className={styles.productTitle}>
+                                  {`${product.productName.slice(0, 20)} ....`}
+                                </Link>
+                                <div className="flex items-start justify-start">
+                                  {!product.smartPrice ? (
+                                    <span>{product.price}</span>
+                                  ) : (
+                                    <span className=" flex items-center text-sm">
+                                      <span className="font-sans text-sm px-1 text-black">
+                                        BTD
+                                      </span>
+                                      {product.smartPrice}
+                                      <span className="mx-1 text-sm line-through text-gray-600">
+                                        {product.price}
+                                      </span>
+                                    </span>
+                                  )}
+                                </div>
+                              </div>
+                            </div>
                           );
                         })}
                       </>
